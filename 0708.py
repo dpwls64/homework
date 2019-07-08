14d22efe756d65f995a5395d72c3571143249f5a
dpwls64 <52019440+dpwls64@users.noreply.github.com>
2019-07-08 14:00:57 +0900

    Update 0708.py

--- 0708.py
+++ 0708.py
@@ -1,30 +1,10 @@
-import sys
+
 
 l = []
 
 for i in range(10):
     print(i+1,"]", end = " ")
     l.append(int(input("Input Number : ")))
-
-
-def my_max(list):
-   
-    max_val = list[0]
-    
-    for j in range(10):
-        if list[j] > max_val:
-            max_val = list[j]
-    
-    return max_val
-
-def my_min(list):
-    min_val = list[0]
-    for j in range(10):
-        
-        if list[j] < min_val:
-            min_val = list[j]
-    
-    return min_val
 
 def my_sum(list):
     sum = 0
@@ -39,7 +19,6 @@ def my_avg(list):
 
     return avg
 
-print(my_max(l))
-print(my_min(l))
+
 print(my_sum(l))
 print(my_avg(l))
